7930418021267522ef547f334a4f11a97843485f
aoi <aoil@hotmail.co.jp>
2021-10-05 13:13:53 +0000

    本番に反映する練習

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    英単語頑張るアプリ
                 </div>
 
                 <div class="links">
