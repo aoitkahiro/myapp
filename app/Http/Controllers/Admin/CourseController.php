2d68794daf5508c5b68e6fc61953b383ea6d57a2
aoi <aoil@hotmail.co.jp>
2021-10-15 04:41:45 +0000

    ランキングがcategoryごとに絞られるように改修した

--- app/Http/Controllers/Admin/CourseController.php
+++ app/Http/Controllers/Admin/CourseController.php
@@ -337,47 +337,39 @@ class CourseController extends Controller
     // dd($forgotten,$request->all());
     return redirect()->action('Admin\CourseController@quiz',['forgotten' => $forgotten]);
   }
-  
   public function ranking()
   {
-    //【まずは】ランキングの関数に入れるカテゴリー絞りの配列を作る
-    $category = "どうぶつの種類";
-    //１．先頭で絞り込む
-    $ranking_array = UserQuizResult::oldest("id")->get();
-    $i = 0;
-    $course_ids_in_ranking =[];
-    foreach($ranking_array as $rank){
-      $course_ids_in_ranking[] = $ranking_array[$i]->course_id;
-      $i++;
-    }
-    // dd($course_ids_in_ranking,$ranking_array[0],$ranking_array[0]->course_id);
-     // coursesテーブルをcourse_idで調べる。
-    $courses_in_this_ranking = Course::where("category",$category)->whereIn("id",$course_ids_in_ranking)->get();
-    dd($courses_in_this_ranking);
-      // もしcourse_idのcategory== $request->categoryならその$course_idには$cat_course_idと名付ける
-      // $cat_course_id を含むレコードと同じuser_id, challenge_id を持っているレコードは、配列に入れる
-    //１．と同じuser_id, challenge_id のresultsがあれば、それを配列に入れる
+
     $courses = Course::all();
     $users = User::all();
     $rankings = [];
+    $category = "TOEIC500";
     foreach ($users as $user) {
       $ary = []; // ここに正解回数が入る([0]が一回目の結果)
       $maxCi = UserQuizResult::where('user_id', $user->id)->max('challenge_id'); // そのユーザのチャレンジID最大値を取得
       for ($ci = 1; $ci <= $maxCi; $ci++) {
-        $success = UserQuizResult::where('user_id', $user->id)->where('challenge_id', $ci)->where('judgement', 2)->count();
-        $modelForTimeAndDate = UserQuizResult::where('user_id', $user->id)->where('challenge_id', $ci)->orderBy('running_time','DESC')->first();
-        // dd($user->id, $ci, $modelForDate);
-        $date = $modelForTimeAndDate->created_at->format('Y/m/d');
-        $running_time = $modelForTimeAndDate->running_time;
-        $mygoal = $user->mygoal;
-        // dd($user->image_path);
-        $ary = ['name' => $user->name, '挑戦回数' => $ci, '正解回数' => $success, '挑戦日'=> $date, 'タイム'=>$running_time, '目標'=>$mygoal, '画像'=>$user->image_path];
-        $rankings[] = $ary;
+        $user_quiz_results = UserQuizResult::where('user_id', $user->id)->where('challenge_id', $ci)->where('judgement', 2)->get();
+        if(count($user_quiz_results) > 0 && Course::find($user_quiz_results[0]->course_id)->category == $category){
+            $modelForTimeAndDate = UserQuizResult::where('user_id', $user->id)->where('challenge_id', $ci)->orderBy('running_time','DESC')->first();
+            // dd($user->id, $ci, $modelForDate);
+            $date = $modelForTimeAndDate->created_at->format('Y/m/d');
+            $running_time = $modelForTimeAndDate->running_time;
+            $mygoal = $user->mygoal;
+            // $date, $success, $running_time　の帳尻が合わないと、ランキングのソートが正常に動かなさそう
+            // success = あるチャレンジの正答数
+            // running_time = あるチャレンジの３問解くのにかかった時間
+            // $date = あるチャレンジの終了日
+            $success = count($user_quiz_results);
+            $ary = ['name' => $user->name, '挑戦回数' => $ci, '正解回数' => $success, '挑戦日'=> $date, 
+            'タイム'=>$running_time, '目標'=>$mygoal, '画像'=>$user->image_path, 'uqz'=> $user_quiz_results];
+            $rankings[] = $ary;
+        }
       }
     }
     $days = array_column($rankings, '挑戦日');
     $numbers = array_column($rankings, '正解回数');
     $times = array_column($rankings, 'タイム');
+    // dd($rankings);
     // $beforeSort = $rankings;
     $result = array_multisort($days, SORT_DESC,$numbers, SORT_DESC, $times, SORT_ASC, $rankings); // 上位以外をはじくために、配列を整える
     // dd($beforeSort,$rankings,$result);
