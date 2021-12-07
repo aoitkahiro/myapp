3e1c12ce1e180575244938125e3de5b900c85349
aoi <aoil@hotmail.co.jp>
2021-12-07 14:21:08 +0000

    Register画面のアプリ名修正

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,7 +25,7 @@
         <nav class="navbar navbar-expand-md navbar-light shadow-sm">
             <div class="container2">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', '単語帳&クイズ') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
