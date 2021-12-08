b4bb7801b29d9745ce2189cf46606f571ac6589f
aoi <aoil@hotmail.co.jp>
2021-12-08 14:58:59 +0000

    login画面にnewUserボタン設置

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -57,12 +57,18 @@
                                     <button type="submit" class="btn btn-primary">
                                         {{ __('Login') }}
                                     </button>
+                                    
     
                                     @if (Route::has('password.request'))
                                         <a class="btn btn-link" href="{{ route('password.request') }}">
                                             {{ __('Forgot Your Password?') }}
                                         </a>
                                     @endif
+                                </div>
+                                <div class="col-md-8 offset-md-4">
+                                    <button href="{{ url('/') }}" class="btn btn-primary" style="margin-top: 20px;">
+                                        New User
+                                    </button>
                                 </div>
                             </div>
                         </form>
