5d219743db3cc9d74cff563ca2ca29b7ea6a6b54
aoi <aoil@hotmail.co.jp>
2021-12-19 03:00:59 +0000

    不要なコードとコメントを削除

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -6,14 +6,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1"> {{-- 小画面時の文字や画像調整タグ --}} 
 
         <!-- CSRF Token -->
-         {{-- 後の章で説明します --}}
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        {{-- 各ページごとにtitleタグを入れるために@yieldで空けておきます。 --}}
+        {{-- 各ページごとにtitleタグを入れるために@yieldで空けておく。 --}}
         <title>@yield('title')</title>
 
         <!-- Scripts -->
-         {{-- Laravel標準で用意されているJavascriptを読み込みます --}}
         <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
         <script src="{{ secure_asset('js/app.js') }}" defer></script>
 
@@ -22,9 +20,7 @@
         <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
 
         <!-- Styles -->
-        {{-- Laravel標準で用意されているCSSを読み込みます --}}
         <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet"> {{-- secure_asset()はpublicディレクトリのパスを返す関数 --}} 
-        {{-- この章の後半で作成するCSSを読み込みます --}}
         <link href="{{ secure_asset('css/admin.css') }}" rel="stylesheet">
         <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
             <img class="gazou" style="max-height:30px;" src="{{ secure_asset('image/' . 'q&t3.png') }}">
@@ -64,7 +60,7 @@
     <body>
         <div id="app"></div>
         <main class="py-4">
-            {{-- コンテンツをここに入れるため、@yieldで空けておきます。 --}}
+            {{-- コンテンツをここに入れるための、@yield --}}
             @yield('content')
         </main>
     @yield('js')
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,10 +27,10 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', '単語帳&クイズ') }}
                 </a>
+                {{--
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                {{--
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
