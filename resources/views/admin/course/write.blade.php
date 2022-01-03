d3502a5cefeb36e818d8ad7bb601099ea63d11a0
aoi <aoil@hotmail.co.jp>
2022-01-03 16:01:19 +0000

    write.bladeのフォントサイズ一部変更

--- resources/views/admin/course/write.blade.php
+++ resources/views/admin/course/write.blade.php
@@ -25,7 +25,7 @@
                             <input type="file" class="form-control-file" name="image" id="myImage" accept="image/*">
                                 <img id="preview" class="image_width">
                             <input type="hidden" name="course_id" value="{{$tango_id_for_write}}">  {{--前のアクションからidを送って→value=に $tango_id_for_writeとして設定  --}} 
-                        <p><button type="submit" >保存</button><p>.jpg か.png<small> 形式の画像ファイルが保存できます</small></p></p>
+                        <p><button type="submit" >保存</button><p><span class="per200_font">.jpg か.png</span><small> 形式の画像ファイルが保存できます</small></p></p>
                     </span>
                 </div>
             </div>
